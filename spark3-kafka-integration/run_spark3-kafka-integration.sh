a8e23a5a5fe646669d20256f1e85d8fbcf1443a4
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-14 13:19:50 +0530

    Update run_spark3-kafka-integration.sh

--- spark3-kafka-integration/run_spark3-kafka-integration.sh
+++ spark3-kafka-integration/run_spark3-kafka-integration.sh
@@ -3,14 +3,13 @@ echo "Running $0 script"
 spark3-submit \
   --master yarn \
   --deploy-mode client \
-  --executor-memory 4g \
+  --executor-memory 1g \
   --num-executors 2 \
-  --driver-memory 4g \
-  --executor-cores 5
+  --driver-memory 1g \
   --files /tmp/kafka_client_jaas.conf \
-  --driver-java-options "-Djava.security.auth.login.config=/tmp/kafka_client_jaas.conf" \
+  --conf "spark.driver.extraJavaOptions=-Djava.security.auth.login.config=/tmp/kafka_client_jaas.conf" \
   --conf "spark.executor.extraJavaOptions=-Djava.security.auth.login.config=/tmp/kafka_client_jaas.conf" \
   --class com.ranga.spark.kafka.SparkKafkaSecureStructuredStreaming \
-  /tmp/SparkCDPHWCExample-1.0.0-SNAPSHOT.jar
+  /tmp/spark3-kafka-integration-1.0.0-SNAPSHOT.jar node1.hadoop.com:9092 SASL_PLAINTEXT KafkaWordCount
   
   echo "Finished $0 script"
