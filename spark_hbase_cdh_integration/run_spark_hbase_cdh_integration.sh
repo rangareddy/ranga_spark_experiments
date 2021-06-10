c570724bed183be6bd1375c998fad744b35693d4
root <root@c266-node3.coelab.cloudera.com>
2021-06-10 08:52:54 +0000

    updated run_spark_hbase_cdh_integration.sh script

--- spark_hbase_cdh_integration/run_spark_hbase_cdh_integration.sh
+++ spark_hbase_cdh_integration/run_spark_hbase_cdh_integration.sh
@@ -2,13 +2,14 @@
 
 echo "Submitting the Spark HBase Integration Application"
 
-spark-submit --master yarn --deploy-mode client \
+sudo -u spark spark-submit \
+  --master yarn \
+  --deploy-mode cluster \
   --driver-memory 1g \
   --executor-memory 2g \
   --executor-cores 5 \
-  --num-executors 2 \
   --class com.ranga.spark.hbase.SparkHBaseIntegrationApp \
-  /usr/apps/spark/spark-hbase/spark-hase-integration-1.0.0-SNAPSHOT.jar
+  /apps/spark/spark-hbase/spark_hbase_cdh_integration-1.0.0-SNAPSHOT.jar
 
 echo "Spark HBase Integration Application completed successfully."
 
