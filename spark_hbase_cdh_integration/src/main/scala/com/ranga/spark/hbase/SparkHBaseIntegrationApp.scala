9108dc9af91701eb6fb8ff431558381add5798d2
root <root@c266-node3.coelab.cloudera.com>
2021-06-10 09:29:14 +0000

    Updated SparkHBaseIntegrationApp.scala

--- spark_hbase_cdh_integration/src/main/scala/com/ranga/spark/hbase/SparkHBaseIntegrationApp.scala
+++ spark_hbase_cdh_integration/src/main/scala/com/ranga/spark/hbase/SparkHBaseIntegrationApp.scala
@@ -18,26 +18,13 @@ object SparkHBaseIntegrationApp extends App with Serializable {
   conf.addResource(new Path("/etc/hbase/conf/hbase-site.xml"))
   new HBaseContext(spark.sparkContext, conf)
 
-  case class Employee(id:Long, name: String, age: Integer, salary: Float)
-  import spark.implicits._
-  
-  var employeeDS = Seq(
-    Employee(1L, "Ranga Reddy", 32, 80000.5f),
-    Employee(2L, "Nishanth Reddy", 3, 180000.5f),
-    Employee(3L, "Raja Sekhar Reddy", 59, 280000.5f)
-  ).toDS()
-
-  val columnMapping = "id Long :key, name STRING e:name, age Integer e:age, salary FLOAT e:salary"
-  val format = "org.apache.hadoop.hbase.spark"
-  val tableName = "employees"
-
-  // Write the data to hbase table
-  employeeDS.write.format(format).option("hbase.columns.mapping", columnMapping).option("hbase.table", tableName).save()
+  val columnMapping = "id STRING :key, name STRING per:name, age STRING per:age, designation STRING prof:designation, salary STRING prof:salary"
+  val tableName = "employee"
 
   // Read the data from hbase table
-  val df = spark.read.format(format).option("hbase.columns.mapping", columnMapping).option("hbase.table", tableName).load()
-  df.printSchema()
-  df.show(truncate=false)
+  val employeeDf = spark.read.format("org.apache.hadoop.hbase.spark").option("hbase.columns.mapping", columnMapping).option("hbase.table", tableName).load()
+  employeeDf.printSchema()
+  employeeDf.show(truncate=false)
 
   // Close the SparkSession
   spark.close()
