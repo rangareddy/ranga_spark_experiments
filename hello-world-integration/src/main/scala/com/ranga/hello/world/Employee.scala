4ce1304a2b063c906d4e4da9862a6739f1781120
Ranga Reddy <rangareddy.avula@gmail.com>
2021-07-13 19:18:37 +0530

    Removing the unused project

--- hello-world-integration/src/main/scala/com/ranga/hello/world/Employee.scala
+++
@@ -1,9 +0,0 @@
-package com.ranga.hello.world
-
-/**
- * @author Ranga Reddy
- * Version: 1.0
- * Created : 07/13/2021
- */
-
-case class Employee(id:Long, name: String, age: Integer, salary: Float)
