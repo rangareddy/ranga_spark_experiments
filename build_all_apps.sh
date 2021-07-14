a22efef61665f3d3c156353844aaaf3303859c8a
Ranga Reddy <rangareddy.avula@gmail.com>
2021-07-14 18:48:56 +0530

    Updated SparkCassandra Project

--- build_all_apps.sh
+++ build_all_apps.sh
@@ -1,13 +1,30 @@
+#!/bin/bash
 
 spark_apps_dir=$(pwd)
-echo "$spark_apps_dir"
-for f in *; do
-    if [ -d "$f" ]; then
-        spark_app_dir=${spark_apps_dir}/$f
-        echo "$spark_app_dir"
+echo ""
+echo "Spark Apps Directory: $spark_apps_dir"
+echo ""
+
+build_logs_dir=${spark_apps_dir}/log
+rm -r -f "${build_logs_dir}"
+mkdir -p "${build_logs_dir}"
+
+for application in *; do
+    if [ -d "$application" ]; then
+        spark_app_dir=${spark_apps_dir}/${application}
         spark_app_pom_path=$spark_app_dir/pom.xml
-        if [ -f $spark_app_pom_path ]; then
-           mvn clean package -DskipTests -f $spark_app_pom_path 
+        if [ -f "$spark_app_pom_path" ]; then
+           spark_app_log=${build_logs_dir}/$application.log
+           mvn clean package -DskipTests -U -f "$spark_app_pom_path" > "${spark_app_log}"
+           build_success=$(cat "${spark_app_log}" | grep -i 'BUILD SUCCESS')
+           if [ -z "$build_success" ]; then
+      		echo ""	
+               	echo "Build is failed for the application ${application} and log location is ${spark_app_log}"
+           else 
+                rm -r -f "${spark_app_log}"
+	   fi 
         fi
     fi
 done
+
+echo ""
--- spark_cassandra_integration/src/main/scala/com/ranga/spark/cassandra/Test.scala
+++
@@ -1,5 +0,0 @@
-package com.ranga.spark.cassandra
-
-object Test {
-
-}
